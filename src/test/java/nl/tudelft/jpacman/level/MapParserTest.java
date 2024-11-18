cf2975261bf104116fee501bad4aaf87b5edac47
Runchida Ananartyasit <runchida.ana@student.mahidol.edu>
2024-11-18 15:20:04 +0700

    test

---
+++ src/test/java/nl/tudelft/jpacman/level/MapParserTest.java
@@ -0,0 +1,71 @@
+package nl.tudelft.jpacman.level;
+
+import nl.tudelft.jpacman.PacmanConfigurationException;
+import nl.tudelft.jpacman.board.BoardFactory;
+import nl.tudelft.jpacman.npc.ghost.Blinky;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.ArrayList;
+
+/**
+ * This is a test class for MapParser.
+ */
+@ExtendWith(MockitoExtension.class)
+public class MapParserTest {
+    @Mock
+    private BoardFactory boardFactory;
+    @Mock
+    private LevelFactory levelFactory;
+    @Mock
+    private Blinky blinky;
+
+    /**
+     * Test for the parseMap method (good map).
+     */
+    @Test
+    public void testParseMapGood() {
+        // No need to manually initialize mocks
+        assertNotNull(boardFactory);
+        assertNotNull(levelFactory);
+
+        Mockito.when(levelFactory.createGhost()).thenReturn(blinky);
+        MapParser mapParser = new MapParser(levelFactory, boardFactory);
+        ArrayList<String> map = new ArrayList<>();
+        map.add("############");
+        map.add("#P        G#");
+        map.add("############");
+        mapParser.parseMap(map);
+
+        Mockito.verify(levelFactory, Mockito.times(1)).createGhost();
+    }
+
+    /**
+     * Test for the parseMap method (bad map).
+     */
+    @Test
+    public void testParseMapWrong1() {
+        PacmanConfigurationException thrown =
+            Assertions.assertThrows(PacmanConfigurationException.class, () -> {
+                assertNotNull(boardFactory);
+                assertNotNull(levelFactory);
+                MapParser mapParser = new MapParser(levelFactory, boardFactory);
+                ArrayList<String> map = new ArrayList<>();
+                    /*
+                    Create a map with inconsistent size between
+                    each row or contain invalid characters
+                    */
+                map.add("############");
+                map.add("#P   SSS  G#");
+                map.add("############");
+                mapParser.parseMap(map);
+            });
+        Assertions.assertEquals("Invalid character at 5,1: S", thrown.getMessage());
+    }
+}
